0fb8372c15d79f7ab81ea8734bf1b23b45674bf7
Tiffany-Morrigan Henebury <timohens@macM1.local>
2025-08-15 21:23:57 +0100

    revert(RevertTest)!: Simple Counter w Test
    Include a simple counter with a test using cargo insta

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,123 @@
-use color_eyre::Result;
-use ratatui::{
-    DefaultTerminal, Frame,
-    crossterm::event::{self, Event, KeyCode, KeyEventKind},
-    layout::{Alignment, Constraint, Layout, Rect},
-    style::{Style, Stylize},
-    text::Line,
-    widgets::{Block, BorderType, Borders, Padding, Paragraph, Wrap},
-};
+// use crate::event::{AppEvent, Event, EventHandler};
+// use ratatui::{
+//     DefaultTerminal,
+//     crossterm::event::{KeyCode, KeyEvent, KeyModifiers},
+// };
+// 
+// pub mod app;
+// pub mod event;
+// pub mod ui;
+// 
+// /// Application.
+// #[derive(Debug)]
+// pub struct App {
+//     /// Is the application running?
+//     pub running: bool,
+//     /// Counter.
+//     pub counter: u8,
+//     /// Event handler.
+//     pub events: EventHandler,
+// }
+// 
+// impl Default for App {
+//     fn default() -> Self {
+//         Self {
+//             running: true,
+//             counter: 0,
+//             events: EventHandler::new(),
+//         }
+//     }
+// }
+// 
+// impl App {
+//     /// Constructs a new instance of [`App`].
+//     pub fn new() -> Self {
+//         Self::default()
+//     }
+// 
+//     /// Run the application's main loop.
+//     pub fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
+//         while self.running {
+//             terminal.draw(|frame| frame.render_widget(&self, frame.area()))?;
+//             self.handle_events()?;
+//         }
+//         Ok(())
+//     }
+// 
+//     pub fn handle_events(&mut self) -> color_eyre::Result<()> {
+//         match self.events.next()? {
+//             Event::Tick => self.tick(),
+//             Event::Crossterm(event) => match event {
+//                 crossterm::event::Event::Key(key_event) => self.handle_key_event(key_event)?,
+//                 _ => {}
+//             },
+//             Event::App(app_event) => match app_event {
+//                 AppEvent::Increment => self.increment_counter(),
+//                 AppEvent::Decrement => self.decrement_counter(),
+//                 AppEvent::Quit => self.quit(),
+//             },
+//         }
+//         Ok(())
+//     }
+// 
+//     /// Handles the key events and updates the state of [`App`].
+//     pub fn handle_key_event(&mut self, key_event: KeyEvent) -> color_eyre::Result<()> {
+//         match key_event.code {
+//             KeyCode::Esc | KeyCode::Char('q') => self.events.send(AppEvent::Quit),
+//             KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => {
+//                 self.events.send(AppEvent::Quit)
+//             }
+//             KeyCode::Right => self.events.send(AppEvent::Increment),
+//             KeyCode::Left => self.events.send(AppEvent::Decrement),
+//             // Other handlers you could add here.
+//             _ => {}
+//         }
+//         Ok(())
+//     }
+// 
+//     /// Handles the tick event of the terminal.
+//     ///
+//     /// The tick event is where you can update the state of your application with any logic that
+//     /// needs to be updated at a fixed frame rate. E.g. polling a server, updating an animation.
+//     pub fn tick(&self) {}
+// 
+//     /// Set running to false to quit the application.
+//     pub fn quit(&mut self) {
+//         self.running = false;
+//     }
+// 
+//     pub fn increment_counter(&mut self) {
+//         self.counter = self.counter.saturating_add(1);
+//     }
+// 
+//     pub fn decrement_counter(&mut self) {
+//         self.counter = self.counter.saturating_sub(1);
+//     }
+// }
+use crate::app::App;
 
-fn main() -> Result<()> {
-    println!("Hello, world!");
+pub mod app;
+pub mod event;
+pub mod ui;
+
+fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
-    let result = run(terminal);
+    let result = App::new().run(terminal);
     ratatui::restore();
     result
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    loop {
-        terminal.draw(draw)?;
-        if let Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                break Ok(());
-            }
-        }
-    }
-}
-
-fn draw(frame: &mut Frame) {
-    let (title_area, layout) = calculate_layout(frame.area());
-
-    render_title(frame, title_area);
-
-    let paragraph = placeholder_paragraph();
-
-    render_padding(&paragraph, frame, layout[0][0]);
-}
-
-fn render_title(frame: &mut Frame, area: Rect) {
-    frame.render_widget(
-        Paragraph::new("Block example. Press q to quit")
-            .dark_gray()
-            .alignment(Alignment::Center),
-        area,
-    );
-}
-
-fn placeholder_paragraph() -> Paragraph<'static> {
-    let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-    Paragraph::new(text.dark_gray()).wrap(Wrap { trim: true })
-}
-
-fn render_padding(paragraph: &Paragraph, frame: &mut Frame, area: Rect) {
-    let block = Block::bordered()
-        .padding(Padding::new(5, 10, 1, 2))
-        .title("Padding");
-    frame.render_widget(paragraph.clone().block(block), area);
-}
-
-/// Calculate the layout of the UI elements.
-///
-/// Returns a tuple of the title area and the main areas.
-fn calculate_layout(area: Rect) -> (Rect, Vec<Vec<Rect>>) {
-    let main_layout = Layout::vertical([Constraint::Length(1), Constraint::Min(0)]);
-    let block_layout = Layout::vertical([Constraint::Max(4); 9]);
-    let [title_area, main_area] = main_layout.areas(area);
-    let main_areas = block_layout
-        .split(main_area)
-        .iter()
-        .map(|&area| {
-            Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)])
-                .split(area)
-                .to_vec()
-        })
-        .collect();
-    (title_area, main_areas)
-}
+// fn run(mut terminal: DefaultTerminal) -> Result<()> {
+//     loop {
+//         terminal.draw(draw)?;
+//         if let Event::Key(key) = event::read()? {
+//             if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+//                 break Ok(());
+//             }
+//         }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
